2e0af216dd197444188bb004d453cdb0d269e241
filipeabessa <filipegbessa@gmail.com>
2024-03-04 20:07:39 -0300

    feat(professionalController): change methods returns

--- src/main/java/com/filipe/bessa/teste/simples/dental/professionals/ProfessionalController.java
+++ src/main/java/com/filipe/bessa/teste/simples/dental/professionals/ProfessionalController.java
@@ -8,13 +8,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,17 +35,18 @@ public class ProfessionalController {
             @PageableDefault(size = 10, sort = {"name"}) Pageable pagination
     ) {
         var professionals = professionalService.getProfessionals(pagination);
-        return ResponseEntity.status(HttpStatus.CREATED).body(professionals);
+        return ResponseEntity.ok(professionals);
     }
 
     @GetMapping("/{id}")
-    public void getProfessional(@PathVariable Long id) {
-        professionalService.getProfessional(id);
+    public ResponseEntity<ProfessionalDetailsDTO> getProfessional(@PathVariable Long id) {
+
+        return ResponseEntity.ok(professionalService.getProfessional(id));
     }
 
-    @PatchMapping()
-    public void updateProfessional(@RequestBody @Valid UpdateProfessionalDTO updateProfessionalDTO) {
-        professionalService.updateProfessional(updateProfessionalDTO);
+    @PutMapping()
+    public ResponseEntity<ProfessionalDetailsDTO> updateProfessional(@RequestBody @Valid UpdateProfessionalDTO updateProfessionalDTO) {
+        return ResponseEntity.ok(professionalService.updateProfessional(updateProfessionalDTO));
     }
 
     @DeleteMapping("/{id}")

fabe86fec70db21f41132f547a53585c3655c88a
filipeabessa <filipegbessa@gmail.com>
2024-03-07 14:54:46 -0300

    feat(DTOs): add DTOs validation

--- src/main/java/com/filipe/bessa/teste/simples/dental/presentation/v1/contacts/dto/CreateContactDTO.java
+++ src/main/java/com/filipe/bessa/teste/simples/dental/presentation/v1/contacts/dto/CreateContactDTO.java
@@ -1,8 +1,13 @@
 package com.filipe.bessa.teste.simples.dental.presentation.v1.contacts.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CreateContactDTO(
+        @NotNull
         Long professionalId,
+        @NotNull
         String name,
+        @NotNull
         String contact
 ) {
 }
--- src/main/java/com/filipe/bessa/teste/simples/dental/presentation/v1/professionals/dto/CreateProfessionalDTO.java
+++ src/main/java/com/filipe/bessa/teste/simples/dental/presentation/v1/professionals/dto/CreateProfessionalDTO.java
@@ -2,16 +2,20 @@ package com.filipe.bessa.teste.simples.dental.presentation.v1.professionals.dto;
 
 import com.filipe.bessa.teste.simples.dental.domain.model.professionals.Position;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record CreateProfessionalDTO(
         @Schema(description = "Professional name", example = "John Doe")
+        @NotNull
         String name,
         @Schema(description = "Professional position", example = "DEVELOPER")
+        @NotNull
         Position position,
 
         @Schema(description = "Professional birth date", example = "01/01/2000")
+        @NotNull
         LocalDate birthDate
 ) {
 }
